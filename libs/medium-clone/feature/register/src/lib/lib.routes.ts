49804dbf7e8bd53d18f2d3f29932f060319ce792
Jake Willmsen <jdwillmsen@gmail.com>
2024-02-04 19:57:10 -0600

    feat: Add register library routing to medium-clone app

---
+++ libs/medium-clone/feature/register/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { MediumCloneFeatureRegisterComponent } from './medium-clone-feature-register/medium-clone-feature-register.component';
+
+export const mediumCloneFeatureRegisterRoutes: Route[] = [
+  { path: '', component: MediumCloneFeatureRegisterComponent },
+];
