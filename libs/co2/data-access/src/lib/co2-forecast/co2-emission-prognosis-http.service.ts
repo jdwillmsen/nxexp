58d8cbd503b13d483f9f08ff7874aa878a5eda10
Jake Willmsen <jdwillmsen@gmail.com>
2024-02-05 21:15:03 -0600

    feat: Add co2-forecast store

--- libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Co2EmissionPrognosisRecord } from './co2-emission-prognosis-record';
 
 const url = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql';
-
-export interface Co2EmissionPrognosisRecord {
-  readonly co2Emission: number;
-  readonly minutes5UTC: Date;
-  readonly priceArea: 'DK1' | 'DK2';
-}
 
 @Injectable({
   providedIn: 'root',
---
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
@@ -0,0 +1,21 @@
+import { Co2ForecastStore } from './co2-forecast.store';
+import { TestBed } from '@angular/core/testing';
+
+describe(Co2ForecastStore.name, () => {
+  function setup() {
+    TestBed.configureTestingModule({
+      providers: [Co2ForecastStore],
+    });
+    const store = TestBed.inject(Co2ForecastStore);
+
+    return {
+      store,
+    };
+  }
+
+  it('is provided externally', () => {
+    const { store } = setup();
+
+    expect(store).not.toBeNull();
+  });
+});
